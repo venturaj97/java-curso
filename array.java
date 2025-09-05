04fbad776f19ec5cc7a65123edf4cbad1c15c9c9
João Victor Ventura Mendes <joaoventura97@outlook.com>
2025-09-05 11:45:05 -0300

    estrutura array

---
+++ array.java
@@ -0,0 +1,15 @@
+public class array {
+
+    public static void main(String[] args) {
+
+    int [] array = new int[5];
+    array [0] = 10;
+    array [1] = 20;
+    System.out.println(array[1]); // saida: 20
+    System.out.println(array[0]); // saida: 10
+
+
+    }
+
+
+}
